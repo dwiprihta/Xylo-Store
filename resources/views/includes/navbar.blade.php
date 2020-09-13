e26de215eeb1f0b1bd68912288aeb3a59005f502
dwi prihtapambudi <prihtapambudi@gmail.com>
2020-09-13 19:51:01 +0700

    star slicing index

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -3,7 +3,7 @@
       data-aos="fade-down"
     >
       <div class="container">
-        <a href="/index.html" class="navbar-brand">
+        <a href="{{route('home')}}" class="navbar-brand">
           <img src="/images/logo.svg" alt="Logo" />
         </a>
         <button
@@ -21,12 +21,12 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item active">
-              <a class="nav-link" href="#"
+            <a class="nav-link" href="{{route('home')}}"
                 >Home <span class="sr-only">(current)</span></a
               >
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="/categories.html">Categories</a>
+              <a class="nav-link" href="{{route('categories')}}">Categories</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="/reward.html">Reward</a>
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'HomeController@index')->name('home');
+Route::get('/categories', 'CategoryController@index')->name('categories');
 
 Auth::routes();
 

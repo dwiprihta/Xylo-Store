90e5db7196bdae9b4c6472eb9b269633973308e3
dwi prihtapambudi <prihtapambudi@gmail.com>
2020-09-17 20:30:55 +0700

    slicing register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,68 @@
-@extends('layouts.app')
+@extends('layouts.auth')
+
+@section('title')
+    Register
+@endsection
 
 @section('content')
-<div class="container">
+<div class="page-content page-auth" id="register">
+    <section class="store-auth" data-aos="fade-up">
+    <div class="container">
+        <div class="row justify-content-center align-items-center row-login mb-5">
+            <div class="col-lg-4">
+                <h2>Cara baru jual beli furniture</h2>
+                <form action="" class="mt-3">
+                    <div class="form-group">
+                        <label for="name">Full Name</label>
+                        <input type="text" class="form-control is-valid" name="name" id="name" v-model="name" autofocus>
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email Address</label>
+                        <input type="email" class="form-control" name="email" id="email">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="text" class="form-control" name="password" id="password" v-model="email">
+                    </div>
+                    <div class="form-group display-inline">
+                        <label for="store">Store</label>
+                        <p class="text-muted">apakah anda juga ingin membuka toko</p>
+                            <div class="custom-control custom-radio custom-control-inline">
+                                <input type="radio" id="openStoreTrue" name="is_store_open" class="custom-control-input" v-model="is_store_open" :value="true">
+                                <label class="custom-control-label" for="openStoreTrue">Ya Boleh </label>
+                            </div>
+                            <div class="custom-control custom-radio custom-control-inline">
+                                <input type="radio" id="openStoreFalse" name="is_store_open" class="custom-control-input" v-model="is_store_open" :value="false">
+                                <label class="custom-control-label" for="openStoreFalse">Enggak dulu deh </label>
+                            </div>
+                    </div>
+                    <div class="form-group" v-if="is_store_open">
+                        <label for="password">Nama Toko</label>
+                        <input type="text" class="form-control" name="password" id="password">
+                    </div>
+                        <div class="form-group" v-if="is_store_open">
+                        <label for="password">Category</label>
+                        <select name="category" class="form-control">
+                            <option value="">All</option>
+                            <option value="">Meja</option>
+                            <option value="">Kursi</option>
+                        </select>
+                    </div>
+                    <button type="submit" name="sumbit" id="btn-add" class="btn btn-success btn-block mt-4">
+                    Sign up Now
+                    </button>
+                    <button class="btn btn-default btn-block">
+                    Back to Sign
+                    </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        </section>
+</div>
+
+
+{{-- <div class="container hide">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -73,5 +134,34 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 @endsection
+
+@push('addon-script')
+    <script src="/vendor/vue/vue.js"></script>
+    <script src="https://unpkg.com/vue-toasted"></script>
+    <script>
+      Vue.use(Toasted);
+      var register= new Vue({
+          el:"#register",
+          mounted(){
+              AOS.init();
+              this.$toasted.error(
+                  "Maaf, email anda sudah terdaftar",
+                  {
+                      position:"top-center",
+                      className:"rounded",
+                      duration:1000,
+                  }
+              );
+          },
+          data:{
+              name:"Dwi Prihtapambudi",
+              email:"dwi@gmail.com",
+              password:"",
+              is_store_open:true,
+              store_name:"",
+          },
+      });
+    </script>
+@endpush
---
+++ resources/views/includes/navbar-auth.blade.php
@@ -0,0 +1,38 @@
+    <nav
+      class="navbar navbar-expand-lg navbar-light navbar-store fixed-top navbar-fixed-top"
+      data-aos="fade-down"
+    >
+      <div class="container">
+        <a href="/index.html" class="navbar-brand">
+          <img src="/images/logo.svg" alt="Logo" />
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="{{url('home')}}"
+                >Home <span class="sr-only">(current)</span></a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{url('categories')}}">Categories</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{url('reward')}}">Reward</a>
+            </li>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
